74c89bca4ce89792a548db6dadf7ca4263eb50b3
Andreas Scherer <andreas_github@freenet.de>
2017-05-05 15:07:48 +0200

    C++11 example for string literals.
    See http://en.cppreference.com/w/cpp/language/string_literal for
    details. Compile with 'g++ -std=c++11'.

---
+++ example.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+ 
+char array1[] = "Foo" "bar";
+// same as
+char array2[] = { 'F', 'o', 'o', 'b', 'a', 'r', '\0' };
+
+const char* s1 = R"foo(
+Hello
+World
+)foo";
+//same as
+const char* s2 = "\nHello\nWorld\n";
+ 
+int main()
+{
+    std::cout << array1 << '\n';
+    std::cout << array2 << '\n';
+      
+    std::cout << s1;
+    std::cout << s2;
+}
